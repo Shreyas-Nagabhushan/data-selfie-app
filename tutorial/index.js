58d4b2287a08ebcaa9fcabe543cd1ee988a6df6f
Shreyas-Nagabhushan <110516607+Shreyas-Nagabhushan@users.noreply.github.com>
2023-06-02 09:23:20 +0530

    using array as a database

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,6 +1,8 @@
 const express = require('express') ;
 const app = express() ;
 
+const database = [] ;
+
 app.listen(3000, () => console.log('listening at port 3000')) ;
 app.use(express.static('public')) ;
 app.use(express.json({ limit : '1mb' })) ;
@@ -8,6 +10,9 @@ app.use(express.json({ limit : '1mb' })) ;
 app.post('/api', (request, response) => {
     console.log('I got a request !')
     console.log(request.body) ;
+    const data = request.body ;
+    database.push(data) ;
+    console.log("number of entries : %d", database.length) ;
     response.json({
         status : 'success', 
         latitude : request.body.lat ,
